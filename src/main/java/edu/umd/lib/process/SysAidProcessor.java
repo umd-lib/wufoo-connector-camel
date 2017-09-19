2cb6b1289fade363f16ee923f26d6c2cb5f48a9a
Mohamed Abdul Rasheed <mohideen@umd.edu>
2017-09-19 15:42:57 -0400

    LIBITD-966. Formatting changes.
    https://issues.umd.edu/browse/LIBITD-966

--- src/main/java/edu/umd/lib/process/SysAidProcessor.java
+++ src/main/java/edu/umd/lib/process/SysAidProcessor.java
@@ -67,11 +67,11 @@ public class SysAidProcessor implements Processor {
 
     if (wufoo_sysaid_map != null && wufoo_sysaid_map.containsKey(message.get("Hash"))) {
       String formMappingProperties = wufoo_sysaid_map.get(message.get("Hash"));
-      log.info("Property File Name :" + formMappingProperties);
+      log.info("Property File Name: " + formMappingProperties);
       sysaid.createServiceRequest(message, formMappingProperties);
     } else {
       log.error("Mapping file for the form:" + message.get("Hash") + " not found.");
-      throw new FormMappingException("Mapping file for the form :" + message.get("Hash") + " not found.");
+      throw new FormMappingException("Mapping file for the form: " + message.get("Hash") + " not found.");
     }
 
   }
